7a7ee2f15c78248674ac2bd31baaf7b106b1d23f
DJA <DJA@DJA-PC>
2017-12-22 18:17:17 +0900

    2017-12-22 크리스마스전 마지막 강의 jsp(과제해야함)

--- iot_jsp/src/com/test/iot/servlet/UserServlet.java
+++ iot_jsp/src/com/test/iot/servlet/UserServlet.java
@@ -2,6 +2,7 @@ package com.test.iot.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -9,31 +10,47 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.test.iot.service.UserService;
+import com.test.iot.service.impl.UserServiceImpl;
+
 public class UserServlet extends HttpServlet {
+	UserService us = new UserServiceImpl();
 	
 	public String getCommand(String uri) {
-		int idx = uri.lastIndexOf(".do");
+		int idx = uri.lastIndexOf("/");
 		if(idx!=-1) {
-			return uri.substring(1, idx);
+			System.out.println("getCommand에 있는것 : "+uri.substring(idx+1));
+			return uri.substring(idx+1);
 		}
 		return "";
 	}
+	
 	@Override
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		doProcess(req, res);
+	}
+	@Override
+	public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+		doProcess(req, res);
+	}
+	
+	
+	public void doProcess(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException{
 		req.setCharacterEncoding("utf-8");
 		res.setCharacterEncoding("utf-8");
 		res.setContentType("text/html;charset=utf-8");
-		String url = req.getRequestURL().toString();
 		String uri = req.getRequestURI();
 		System.out.println("uri = "+uri);
 		String cmd = getCommand(uri);
 		if(cmd.equals("list")) {
+			req.setAttribute("list", us.getUserList());
 		}else if(cmd.equals("view")) {
+			//us.getUser();
 		}else {
 			cmd = "/common/error";
 		}
-		cmd += ".jsp";
-		RequestDispatcher rd = req.getRequestDispatcher(cmd);
+		uri = "/WEB-INF/view"+uri+".jsp";
+		RequestDispatcher rd = req.getRequestDispatcher(uri);
 		rd.forward(req, res);
 	}
 		
