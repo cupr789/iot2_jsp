bc33fd50fedcf32f21cb2bf30967933c537c09b4
DJA <DJA@DJA-PC>
2017-12-22 18:17:58 +0900

    2017-12-22 크리스마스전 마지막강의 (과제요망) 그전꺼잘못올려서 다시올리는거임

---
+++ iot_jsp/src/com/test/iot/common/DBCon.java
@@ -0,0 +1,31 @@
+package com.test.iot.common;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+
+
+public class DBCon {
+    private static Connection conn;
+    public static Connection getCon() {
+    	if(DBCon.conn==null) {
+    		Context initContext;
+    		try {
+    		initContext = new InitialContext();
+    		Context envContext = (Context)initContext.lookup("java:/comp/env");
+    		DataSource ds = (DataSource)envContext.lookup("jdbc/IOT");
+    		DBCon.conn =  ds.getConnection();
+    		}catch(NamingException | SQLException e) {
+    			System.out.println("컨넥션연결실패");
+    			e.printStackTrace();
+    		}
+    	}
+    	System.out.println(conn);
+    	return DBCon.conn;
+    }
+}
